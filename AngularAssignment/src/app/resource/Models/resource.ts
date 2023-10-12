da9c34559996aaf03d18d5e3fb7a43ac4ca26502
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-10-13 02:21:19 +0300

    added resource interface

--- AngularAssignment/src/app/resource/Models/resource.ts
+++ AngularAssignment/src/app/resource/Models/resource.ts
@@ -1,3 +1,14 @@
 export interface Resource {
-    
+    name: string,
+    img: string,
+    allDay: boolean,
+    someDays: boolean,
+    months: Array<number> [],
+    timeService: boolean,
+    fixedTime: boolean,
+    bookedPerson: boolean,
+    moreBookedPerson: number,
+    workedDays: Array<string> [] 
+    startedTime: Date,
+    endedTime: Date
 }
